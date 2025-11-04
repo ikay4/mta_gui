80522b6ce2dcdc53a60246b944e5cb704d0c7207
ikay <kye18282@gmail.com>
2025-11-03 18:59:42 -0600

    init commit

---
+++ build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_mta_gui_FILE C:/Users/Kye18/Documents/QTProjects/mta_gui/mta_gui/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/mta_gui.exe)
+set(__QT_DEPLOY_TARGET_mta_gui_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_mta_gui_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
